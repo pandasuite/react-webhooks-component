946165e9507c78c3d4a8799768895af03e162240
Benjamin Barbe <benjamin@pandasuite.com>
2023-06-06 12:00:07 +0200

    Remove unregister of service worker

--- src/index.js
+++ src/index.js
@@ -6,13 +6,7 @@ import ReactDOM from 'react-dom';
 import { PandaBridgeRoot } from 'pandasuite-bridge-react';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 import IntlProvider from './components/IntlProvider';
 
 ReactDOM.render(<PandaBridgeRoot><IntlProvider><App /></IntlProvider></PandaBridgeRoot>, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
